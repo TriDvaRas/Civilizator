5abc15248b7bbef07b21a73d8ef3df98616bc0a1
TriDvaRas <02dimon02@gmail.com>
2020-07-23 00:18:16 +0300

    added civRole permission ignore for Civlizator Guild

--- assets/functions/Permissions.js
+++ assets/functions/Permissions.js
@@ -10,11 +10,14 @@ module.exports = {
                 if (`${gMember.user}` == "<@272084627794034688>") return resolve();
 
                 if (allow) {
+                    //check if admin
                     if (allow.admin && gMember.permissions.any('ADMINISTRATOR'))
                         return resolve();
+                    //check if op
                     if (allow.op && (op == `${gMember.user}`))
                         return resolve();
-                    if (allow.civ && (gMember.roles.cache.some(role => role.id == config.roleId)))
+                    //check if civilized or civlizator guild
+                    if (allow.civ && (gMember.roles.cache.some(role => role.id == config.roleId) || gMember.guild.id == "727081958823165963"))
                         return resolve();
                 }
 
