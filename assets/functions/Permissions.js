c3bb7f6ad6808326b2cb44f7f76ed4d4898cff49
TriDvaRas <02dimon02@gmail.com>
2020-08-10 16:53:55 +0300

    Update Permissions.js

--- assets/functions/Permissions.js
+++ assets/functions/Permissions.js
@@ -7,21 +7,25 @@ module.exports = {
             GC.getConfig(gMember.guild).then(config => {
 
                 //botOwner
-                if (`${gMember.user}` == "<@272084627794034688>") return resolve();
-
-                if (allow) {
+                if (`${gMember.user}` == "<@272084627794034688>")
+                    resolve();
+                else if (allow) {
                     //check if admin
                     if (allow.admin && gMember.permissions.any('ADMINISTRATOR'))
-                        return resolve();
+                        resolve();
                     //check if op
-                    if (allow.op && (op == `${gMember.user}`))
-                        return resolve();
+                    else if (allow.op && (op == `${gMember.user}`))
+                        resolve();
                     //check if civilized or civlizator guild
-                    if (allow.civ && (gMember.roles.cache.some(role => role.id == config.roleId) || gMember.guild.id == "727081958823165963"))
-                        return resolve();
+                    else if (allow.civ && (gMember.roles.cache.some(role => role.id == config.roleId) || gMember.guild.id == "727081958823165963"))
+                        resolve();
+                    else
+                        reject();
                 }
+                else
+                    reject();
 
-                return reject();
+
             });
         });
 
