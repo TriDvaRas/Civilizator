5df88631f9052a1d9f458925da7acfc1a6fbc244
TriDvaRas <02dimon02@gmail.com>
2020-12-11 19:13:19 +0200

    fcs command

---
+++ commands/forceSync.js
@@ -0,0 +1,15 @@
+const db = require("../functions/db");
+
+module.exports = {
+    name: 'fcs',
+    description: `fcs(bot owner only) (you shouldn't see this btw, pls send smth like \`i can see fcs\` here)`,
+    ignore: true,
+    usage: '`forceSync`',
+    execute: async function (message, args) {
+        if (message.author.id == 272084627794034688){
+            db.getUnsynced().then(() => {message.reply(`suc`)},() => {message.reply(`err`)})
+            message.reply(`started fcs`)
+            
+        }
+    },
+};
